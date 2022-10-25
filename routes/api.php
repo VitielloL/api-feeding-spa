acd2f2fc20ba6d9fcaebce2a1003e8fd78dc8d78
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-10-25 18:26:32 -0300

    fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -26,28 +26,28 @@ Route::prefix('city')->group(function (){
 });
 
 Route::prefix('group')->group(function ( ) {
-    Route::get('/show/{group}',[GroupController::class,'show'])->name('group');
+    Route::get('/{group}',[GroupController::class,'show'])->name('group');
     Route::post('/store',[GroupController::class,'store'])->name('group.store');
     Route::post('/update/{group}',[GroupController::class,'update'])->name('group.update');
     Route::post('/delete/{group}',[GroupController::class,'delete'])->name('group.delete');
 });
 
 Route::prefix('campaign')->group(function ( ) {
-    Route::get('/show/{campaign}',[CampaignController::class,'show'])->name('campaign');
+    Route::get('/{campaign}',[CampaignController::class,'show'])->name('campaign');
     Route::post('/store',[CampaignController::class,'store'])->name('campaign.store');
     Route::post('/update/{campaign}',[CampaignController::class,'update'])->name('campaign.update');
     Route::post('/delete/{campaign}',[CampaignController::class,'delete'])->name('campaign.delete');
 });
 
 Route::prefix('product')->group(function ( ) {
-    Route::get('/show/{product}',[ProductController::class,'show'])->name('product');
+    Route::get('/{product}',[ProductController::class,'show'])->name('product');
     Route::post('/store',[ProductController::class,'store'])->name('product.store');
     Route::post('/update/{product}',[ProductController::class,'update'])->name('product.update');
     Route::post('/delete/{product}',[ProductController::class,'delete'])->name('product.delete');
 });
 
 Route::prefix('discount')->group(function ( ) {
-    Route::get('/show/{discount}',[DiscountController::class,'show'])->name('discount');
+    Route::get('/{discount}',[DiscountController::class,'show'])->name('discount');
     Route::post('/store',[DiscountController::class,'store'])->name('discount.store');
     Route::post('/update/{discount}',[DiscountController::class,'update'])->name('discount.update');
     Route::post('/delete/{discount}',[DiscountController::class,'delete'])->name('discount.delete');
