bc97aa9d2d46448ff829372e69309ad30e6a08fd
Lucas Vitiello <40008072+VitielloL@users.noreply.github.com>
2022-10-25 14:42:37 -0300

    fix: routes delete

--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,12 @@ Route::prefix('city')->group(function (){
     Route::get('/',[CityController::class,'index'])->name('city');
     Route::post('/store',[CityController::class,'store'])->name('city.store');
     Route::post('/update',[CityController::class,'update'])->name('city.update');
-    Route::post('/delete',[CityController::class,'update'])->name('city.delete');
+    Route::post('/delete',[CityController::class,'delete'])->name('city.delete');
 });
 
 Route::prefix('group')->group(function ( ) {
     Route::get('/',[GroupController::class,'index'])->name('group');
     Route::post('/store',[GroupController::class,'store'])->name('group.store');
     Route::post('/update',[GroupController::class,'update'])->name('group.update');
-    Route::post('/delete',[GroupController::class,'update'])->name('group.delete');
+    Route::post('/delete',[GroupController::class,'delete'])->name('group.delete');
 });
